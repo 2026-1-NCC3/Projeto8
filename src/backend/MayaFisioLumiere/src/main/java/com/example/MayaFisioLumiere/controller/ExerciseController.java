674df7e49410b6dc8f935a6e37c280f9dd6fd9b0
Sofia B. Hernandes <sofiahernandes.dev@gmail.com>
2026-03-09 19:35:06 -0300

    refactor: Formatting in ExerciseController

--- src/backend/MayaFisioLumiere/src/main/java/com/example/MayaFisioLumiere/controller/ExerciseController.java
+++ src/backend/MayaFisioLumiere/src/main/java/com/example/MayaFisioLumiere/controller/ExerciseController.java
@@ -16,73 +16,74 @@ public class ExerciseController {
     @Autowired
     private ExerciseService exerciseService;
 
-    //criar exercicios novos
-    //rota http://localhost:3000/api/exercise
+    // Criar exercícios novos
+    // http://localhost:3000/api/exercise
     @PostMapping("/create-exercise")
-    public ResponseEntity<ExerciseEntity> create(@RequestBody ExerciseRequestDTO body){
+    public ResponseEntity<ExerciseEntity> create(@RequestBody ExerciseRequestDTO body) {
         ExerciseEntity newExercise = this.exerciseService.createExercise(body);
         return ResponseEntity.ok(newExercise);
     }
 
-    //atualizar o exercicio
-    //http://localhost:3000/api/exercise/updateExercise/8
+    // Atualizar o exercício
+    // http://localhost:3000/api/exercise/updateExercise/8
     @PutMapping("/updateExercise/{id}")
     public ResponseEntity<ExerciseResponseDTO> updateExercise(
             @PathVariable Long id,
-            @RequestBody ExerciseRequestDTO data){
+            @RequestBody ExerciseRequestDTO data) {
         ExerciseResponseDTO exercise = exerciseService.updateExercise(id, data);
         return ResponseEntity.ok(exercise);
     }
 
-    //retornar os exercicios cadastrados, com paginação, para não retornar TODOS os exercicios e uma vez,
-// e sim alguns por pagina e depois scrollar ou clicar no +1 pagina
-    //rota http://localhost:3000/api/exercise/all?page=2&size=3,
+    // Retornar os exercícios cadastrados, com paginação, para não retornar TODOS os
+    // exercícios e uma vez, e sim alguns por página e depois scrollar ou clicar no +1 página
+    // http://localhost:3000/api/exercise/all?page=2&size=3,
     @GetMapping("/all")
-    public ResponseEntity<List<ExerciseResponseDTO>> getExercises(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size){
+    public ResponseEntity<List<ExerciseResponseDTO>> getExercises(@RequestParam(defaultValue = "0") int page,
+                                                                  @RequestParam(defaultValue = "10") int size) {
         List<ExerciseResponseDTO> allExercises = this.exerciseService.getAllExercises(page, size);
         return ResponseEntity.ok(allExercises);
     }
 
     // Buscar por título exato
-    //Rota http://localhost:3000/api/exercise/searchByTitle/flexão
+    // http://localhost:3000/api/exercise/searchByTitle/flexão
 
-    // Buscar por título, ignora letra maiuscula, minuscula, acentos, consegue pesquisar por letra unica
+    // Buscar exercício por título, ignora letra maiúscula, minúscula, acentos,
+    // consegue pesquisar por letra única
     @GetMapping("/searchByTitle/{title}")
     public ResponseEntity<List<ExerciseResponseDTO>> searchByTitle(
-            @PathVariable String title){
+            @PathVariable String title) {
 
         List<ExerciseResponseDTO> exercises = exerciseService.findByTitleContaining(title);
         return ResponseEntity.ok(exercises);
     }
 
-    //procurar exercicio por id
-    //rota http://localhost:3000/api/exercise/5
+    // Procurar exercício por id
+    // http://localhost:3000/api/exercise/5
     @GetMapping("/{id}")
     public ResponseEntity<ExerciseResponseDTO> getExerciseById(
-            @PathVariable Long id){
+            @PathVariable Long id) {
 
         ExerciseResponseDTO exercise = exerciseService.findById(id);
 
         return ResponseEntity.ok(exercise);
     }
 
-    // Buscar por tag
-    //http://localhost:3000/api/exercise/tag/superiores
+    // Buscar exercício por tag
+    // http://localhost:3000/api/exercise/tag/superiores
     @GetMapping("/tag/{tag}")
     public ResponseEntity<List<ExerciseResponseDTO>> searchByTag(
-            @PathVariable String tag){
+            @PathVariable String tag) {
 
         List<ExerciseResponseDTO> exercises = exerciseService.findByTags(tag);
         return ResponseEntity.ok(exercises);
     }
 
-    //deletar exercicio por id
-    //rota  http://localhost:3000/api/exercise/deleteExerciseId/5
+    // Deletar exercício por id
+    // http://localhost:3000/api/exercise/deleteExerciseId/5
     @DeleteMapping("/deleteExerciseId/{id}")
-    public ResponseEntity<String> deleteExercise(@PathVariable Long id){
+    public ResponseEntity<String> deleteExercise(@PathVariable Long id) {
 
         exerciseService.deleteExerciseById(id);
         return ResponseEntity.ok("Exercicio deletado com sucesso");
     }
-
 }
