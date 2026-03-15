7582f6b3917d0d09fb21e0442cdd32497de19c26
Analice Coimbra <analicecoimbra2000@gmail.com>
2026-03-14 23:04:11 -0300

    Fix/unique-email-AdminEntity

--- src/backend/MayaFisioLumiere/src/main/java/com/example/MayaFisioLumiere/entity/AdminEntity.java
+++ src/backend/MayaFisioLumiere/src/main/java/com/example/MayaFisioLumiere/entity/AdminEntity.java
@@ -21,7 +21,8 @@ public class AdminEntity {
     @Column(nullable = false)
     private String adminName;
 
-    @Column(nullable = false)
+
+    @Column(nullable = false, unique = true)
     private String adminEmail;
 
     @Column(nullable = false)
