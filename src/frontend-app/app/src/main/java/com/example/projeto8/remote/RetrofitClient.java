c5c7bc8d51a6f8703b43dccb44015c496d531467
Analice Coimbra <analicecoimbra2000@gmail.com>
2026-03-28 20:49:20 -0300

    feat: add generic retrofitClient

--- src/frontend-app/app/src/main/java/com/example/projeto8/remote/RetrofitClient.java
+++ src/frontend-app/app/src/main/java/com/example/projeto8/remote/RetrofitClient.java
@@ -1,5 +1,7 @@
 package com.example.projeto8.remote;
 
+import com.example.projeto8.api.exerciseSession.ExerciseSessionService;
+import com.example.projeto8.api.patient.PatientService;
 import com.example.projeto8.api.workout.WorkoutService;
 
 import retrofit2.Retrofit;
@@ -7,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     // 1. Aqui vai o link que o Render te deu (precisa terminar com /)
-    private static final String BASE_URL = "https://projeto8.onrender.com/";
+  /*  private static final String BASE_URL = "https://projeto8.onrender.com/";
     private static Retrofit retrofit = null;
 
     public static WorkoutService getWorkoutService() {
@@ -19,4 +21,33 @@ public class RetrofitClient {
         }
         return retrofit.create(WorkoutService.class);
     }
+
+    */
+        private static final String BASE_URL = "https://projeto8.onrender.com/";
+        private static Retrofit retrofit = null;
+
+        // Dessa forma, é necessário criar o retrofit builder só uma vez invés de para cada rota!!
+        private static Retrofit getRetrofitInstance() {
+            if (retrofit == null) {
+                retrofit = new Retrofit.Builder()
+                        .baseUrl(BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+            }
+            return retrofit;
+        }
+
+        //Interfaces
+        public static WorkoutService getWorkoutService() {
+            return getRetrofitInstance().create(WorkoutService.class);
+        }
+
+        public static PatientService getPatientService() {
+            return getRetrofitInstance().create(PatientService.class);
+        }
+
+        public static ExerciseSessionService getExerciseService() {
+            return getRetrofitInstance().create(ExerciseSessionService.class);
+        }
+
 }
