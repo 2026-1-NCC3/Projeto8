70ef6b02fa14b268d28932e965603b589d8adcc0
Analice Coimbra <analicecoimbra2000@gmail.com>
2026-03-28 20:51:07 -0300

    add: PatientService initial setup

--- src/frontend-app/app/src/main/java/com/example/projeto8/api/patient/PatientService.java
+++ src/frontend-app/app/src/main/java/com/example/projeto8/api/patient/PatientService.java
@@ -1,5 +1,10 @@
 package com.example.projeto8.api.patient;
 
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
 public interface PatientService {
     //para pegar as rotas do backend de patients (dados do patient)!
+
 }
